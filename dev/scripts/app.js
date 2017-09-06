64c270e73c0346a79372265878e3ffb96a5f7c18
Emma Kinch <eokinch@gmail.com>
2017-09-06 10:31:15 -0400

    updating basic structure

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,1 +1,12 @@
-console.log(`It's WORKING!!!`)
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	render(){
+		return(
+			<h1>HELLO!!!</h1>
+			)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
